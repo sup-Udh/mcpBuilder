2ef93f00dc4ebdff63718928f2523ae6f652ffca
udhay rajeev <57910021+sup-Udh@users.noreply.github.com>
2026-05-14 10:19:55 +0530

    bge-model

--- lib/embeddings/proivders/bge.ts
+++ lib/embeddings/proivders/bge.ts
@@ -3,3 +3,39 @@
 // text
 // ↓
 // vector
+
+import { pipeline } from "@xenova/transformers";
+
+
+
+let extractor: any = null;
+
+
+export async function loadBGEModel() {
+  if (!extractor) {
+    console.log('Loading BGE embedding model...');
+
+    extractor = await pipeline(
+      'feature-extraction',
+      'Xenova/BAAI-bge-small-en-v1.5'
+    );
+
+    console.log('BGE model loaded');
+  }
+
+  return extractor;
+}
+
+// Single text embedding method 
+export async function embedText(
+  text: string
+): Promise<number[]> {
+  const model = await loadBGEModel();
+
+  const output = await model(text, {
+    pooling: 'mean',
+    normalize: true,
+  });
+
+  return Array.from(output.data);
+}
